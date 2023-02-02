5803c746c871cf7bae664ac95123bb59b65d467a
Niko Helle <niko.hele@hyperactive.fi>
2023-02-02 18:55:56 +0200

    Simplify stories
    Group stories and use argTypes to switch layouts.

--- packages/react/src/components/hero/Hero.stories.tsx
+++ packages/react/src/components/hero/Hero.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Hero } from './Hero';
+import { Hero, HeroProps } from './Hero';
 import { Button } from '../button/Button';
 // @ts-ignore
 import imageFile from '../../assets/img/placeholder_1920x1080.jpg';
@@ -14,164 +14,129 @@ export default {
   args: {},
 };
 
-export const TextAndImage = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#c2a251', '--color': '#000' }} imageAspectRatio="16:9">
-    <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
+type DefaultCardContentProps = {
+  title?: string;
+  text?: string;
+  buttonStyle?: Record<string, string>;
+};
+const defaultText =
+  'Nullam ut nunc consectetur, accumsan nunc sed, luctus nisl. Curabitur lacinia tristique est, sit amet egestas velit elementum sit amet. Nam lacinia volutpat erat vel faucibus.';
+const DefaultCardContent = (props: DefaultCardContentProps) => {
+  const { title, text, buttonStyle } = props;
+  const h1Text = title || 'Welcome to the page hero';
+  const paragraphText = text || defaultText;
+  return (
+    <>
+      <h1>{h1Text}</h1>
+      <p>{paragraphText}</p>
       <Button
         variant="secondary"
         role="link"
         // @ts-ignore
-        style={{ '--background-color': '#000', '--color': '#fff', '--border-color': '#000' }}
+        style={buttonStyle}
       >
-        Button
+        Click me
       </Button>
-    </Hero.Card>
-    <Hero.Image src={imageFile} />
-  </Hero>
-);
-export const ImageAndText = (args) => (
-  <Hero {...args} theme={{ '--background-color': 'var(--color-bus)', '--color': '#fff' }} imageAspectRatio="16:9">
-    <Hero.Image src={imageFile} />
+    </>
+  );
+};
+
+export const ImageLeftOrRight = (args) => (
+  <Hero theme={{ '--background-color': '#c2a251', '--color': '#000' }} imageAspectRatio="16:9">
+    {args?.imagePosition === 'left' && <Hero.Image src={imageFile} />}
     <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button
-        variant="secondary"
-        role="link"
-        // @ts-ignore
-        style={{ '--background-color': '#fff', '--color': '#000', '--border-color': '#fff' }}
-      >
-        Button
-      </Button>
+      <DefaultCardContent buttonStyle={{ '--background-color': '#000', '--color': '#fff', '--border-color': '#000' }} />
     </Hero.Card>
+    {args?.imagePosition === 'right' && <Hero.Image src={imageFile} />}
   </Hero>
 );
 
-export const CenteredTextCard = (args) => (
-  <Hero
-    {...args}
-    theme={{ '--background-color': '#9fc9eb', '--color': '#000', '--koros-color': '#009246' }}
-    koros={{ type: 'storm', forcedDirection: 'up' }}
-  >
-    <Hero.Card centered>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button variant="secondary" role="link">
-        Button
-      </Button>
-    </Hero.Card>
-  </Hero>
-);
+ImageLeftOrRight.argTypes = {
+  imagePosition: {
+    options: ['right', 'left'],
+    control: { type: 'radio' },
+    defaultValue: 'right',
+  },
+};
 
-export const NoImage = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#000', '--color': '#fff' }}>
-    <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button
-        variant="secondary"
-        role="link"
-        // @ts-ignore
-        style={{ '--background-color': '#fff', '--color': '#000', '--border-color': '#fff' }}
-      >
-        Button
-      </Button>
-    </Hero.Card>
-  </Hero>
-);
-
-export const NoKoros = (args) => (
-  <div style={{ backgroundColor: '#ccc', padding: '60px 10px' }}>
-    <Hero {...args} koros={{ hide: true }}>
-      <Hero.Card>
-        <h1>This is a hero header</h1>
-        <p>This is a hero text with lorem ipsum</p>
-        <Button variant="secondary" role="link">
-          Button
-        </Button>
+export const WithoutImage = (args) => {
+  const heroProps: HeroProps = {};
+  const defaultContentProps: DefaultCardContentProps = {};
+  if (args.heroType === 'blueAndGreen') {
+    heroProps.theme = { '--background-color': '#9fc9eb', '--color': '#000', '--koros-color': '#009246' };
+    heroProps.koros = { type: 'storm', forcedDirection: 'up' };
+  } else if (args.heroType === 'blackAndWhite') {
+    heroProps.theme = { '--background-color': '#000', '--color': '#fff' };
+    defaultContentProps.buttonStyle = { '--background-color': '#fff', '--color': '#000', '--border-color': '#fff' };
+  } else if (args.heroType === 'whiteWithoutKoros') {
+    heroProps.koros = { hide: true };
+    heroProps.theme = { '--background-color': '#fff', '--color': '#000' };
+    defaultContentProps.buttonStyle = {};
+  }
+  return (
+    <Hero {...heroProps}>
+      <Hero.Card centered={args.heroType === 'blueAndGreen'}>
+        <DefaultCardContent {...defaultContentProps} />
       </Hero.Card>
     </Hero>
-  </div>
-);
+  );
+};
 
-export const WithTopBackgroundImage = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#ccc', '--color': '#000', '--koros-color': '#fff' }}>
-    <Hero.BackgroundImage src={imageFile} />
-    <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button variant="secondary" role="link">
-        Button
-      </Button>
-    </Hero.Card>
-  </Hero>
-);
+WithoutImage.argTypes = {
+  heroType: {
+    options: ['blackAndWhite', 'blueAndGreen', 'whiteWithoutKoros'],
+    control: { type: 'radio' },
+    defaultValue: 'blueAndGreen',
+  },
+};
 
-export const WithTopBackgroundImageCustomKoros = (args) => (
-  <div style={{ backgroundColor: '#c2a251', padding: '60px 10px' }}>
-    <Hero
-      {...args}
-      theme={{ '--background-color': '#fff', '--color': '#000', '--koros-color': '#c2a251' }}
-      koros={{ type: 'pulse' }}
-    >
-      <Hero.BackgroundImage src={imageFile} />
+export const WithBackgroundImage = (args) => {
+  const heroProps: HeroProps = {};
+  if (args.imagePosition === 'top') {
+    heroProps.theme = { '--background-color': '#ccc', '--color': '#000', '--koros-color': '#fff' };
+  } else if (args.imagePosition === 'right') {
+    heroProps.theme = { '--background-color': '#f5a3c7', '--color': '#000' };
+  }
+
+  return (
+    <Hero {...heroProps}>
+      {args?.imagePosition === 'top' && <Hero.BackgroundImage src={imageFile} />}
       <Hero.Card>
-        <h1>This is a hero header</h1>
-        <p>This is a hero text with lorem ipsum</p>
-        <Button variant="secondary" role="link">
-          Button
-        </Button>
+        {!args.demoLongContent ? (
+          <DefaultCardContent
+            buttonStyle={{ '--background-color': '#000', '--color': '#fff', '--border-color': '#000' }}
+          />
+        ) : (
+          <Hero.Card>
+            <h1>This is a header with too much text for single line</h1>
+            <p>{defaultText}</p>
+            <p>{defaultText}</p>
+            <Button variant="secondary" role="link">
+              Click me once!
+            </Button>
+            <p>
+              <Button variant="secondary" role="link">
+                Never click me!
+              </Button>
+            </p>
+          </Hero.Card>
+        )}
       </Hero.Card>
+      {args?.imagePosition === 'right' && <Hero.BackgroundImage src={imageFile} />}
     </Hero>
-  </div>
-);
+  );
+};
 
-export const WithAngledBackgroundImage = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#f5a3c7', '--color': '#000' }}>
-    <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button variant="secondary" role="link">
-        Button
-      </Button>
-    </Hero.Card>
-    <Hero.BackgroundImage src={imageFile} />
-  </Hero>
-);
-export const WithAngledBackgroundImageVersion2 = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#f5a3c7', '--color': '#000' }}>
-    <Hero.Card>
-      <h1>This is a hero header with too much text for single line</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <p>This is a hero text with lorem ipsum</p>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button variant="secondary" role="link">
-        Button
-      </Button>
-      <p>
-        <Button variant="secondary" role="link">
-          Button
-        </Button>
-      </p>
-    </Hero.Card>
-    <Hero.BackgroundImage src={imageFile} />
-  </Hero>
-);
-
-export const WideImageVersion = (args) => (
-  <Hero {...args} theme={{ '--background-color': '#fff', '--image-position': 'bottom left' }} imageAspectRatio="1:1">
-    <Hero.Card>
-      <h1>This is a hero header</h1>
-      <p>This is a hero text with lorem ipsum</p>
-      <Button variant="secondary" role="link">
-        Button
-      </Button>
-    </Hero.Card>
-    <Hero.WideImage src={imageFile} />
-  </Hero>
-);
+WithBackgroundImage.argTypes = {
+  imagePosition: {
+    options: ['top', 'right'],
+    control: { type: 'radio' },
+    defaultValue: 'top',
+  },
+  demoLongContent: {
+    control: 'boolean',
+  },
+};
 
 export const WithElementAttributes = (args) => (
   <Hero
